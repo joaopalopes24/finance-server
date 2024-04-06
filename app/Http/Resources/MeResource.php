bdfbd05520f69a1f3f4ec544713a159529b1cffa
João Pedro Lopes <joaopalopes24@gmail.com>
2024-04-05 23:07:39 -0300

    test(me): adding test to me endpoint

--- app/Http/Resources/MeResource.php
+++ app/Http/Resources/MeResource.php
@@ -12,7 +12,14 @@ class MeResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'email_verified_at' => $this->email_verified_at,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
     }
 
     /**
---
+++ tests/Feature/MeTest.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Testing\Fluent\AssertableJson;
+use Laravel\Sanctum\Sanctum;
+
+beforeEach(function () {
+    $this->user = User::factory()->create();
+
+    Sanctum::actingAs($this->user);
+});
+
+test('it should return the authenticated user', function () {
+    $response = $this->getJson('/api/me');
+
+    $response->assertOk()->assertMessage(trans('messages.me'));
+
+    $response->assertJson(function (AssertableJson $json) {
+        $json->has('data', function (AssertableJson $json) {
+            $json->where('id', $this->user->id);
+
+            $json->hasAll(['id', 'name', 'email', 'email_verified_at', 'created_at', 'updated_at']);
+        })->has('message');
+    });
+});
